073226a4dcc4f9f2c0c2df0f3b7a690b043d77a1
Ridak08 <123121141+Ridak08@users.noreply.github.com>
2024-06-02 18:04:51 -0400

    Update model.py

--- train/model.py
+++ train/model.py
@@ -37,8 +37,6 @@ def train_and_eval(
     features = df_shuffle.drop(" Label", axis=1).values
     labels = df_shuffle[" Label"].values
     
-    X = df_shuffle.data
-    y = df_shuffle.target
     X_train, X_test, y_train, y_test = train_test_split(features, labels, test_size=test_size, random_state=random_state)
     
     features = X_train.shape[1]
@@ -58,7 +56,7 @@ def train_and_eval(
     model_dnn_1.add(Dense(nClasses, activation='softmax'))                  #Capa de salida
 
     model_dnn_1.compile(optimizer='adam', loss= 'sparse_categorical_crossentropy', metrics= ['accuracy'])
-    model_dnn_1.fit(X_train, y_train, epochs = num_epochs)
+    model_dnn_1.fit(X_train, y_train, validation_data=(X_test, y_test))
     
     y_pred = model_dnn_1.predict(X_test)
     accuracy = metrics.accuracy_score(y_test, y_pred)
