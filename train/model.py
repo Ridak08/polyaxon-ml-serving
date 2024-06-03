00719e6c5714e84febeaf748bbf2789b6e7338c9
Ridak08 <123121141+Ridak08@users.noreply.github.com>
2024-06-03 11:36:33 -0400

    Update model.py

--- train/model.py
+++ train/model.py
@@ -8,8 +8,9 @@ from sklearn.datasets import load_iris
 
 import pandas as pd
 import numpy as np
-#import matplotlib.pyplot as plt
-#import sklearn
+import sklearn
+import matplotlib.pyplot as plt
+
 #from sklearn.svm import SVC
 
 #from tensorflow.keras.utils import to_categorical, plot_model
