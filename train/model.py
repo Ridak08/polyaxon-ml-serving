8cd5ca7ee421c968c079f51befd6091766c4c90e
Ridak08 <123121141+Ridak08@users.noreply.github.com>
2024-06-03 11:23:29 -0400

    Update model.py

--- train/model.py
+++ train/model.py
@@ -1,32 +1,25 @@
 #Importacion de librerias
+import joblib
+from sklearn.model_selection import train_test_split
+from sklearn import metrics
+
+from sklearn.neighbors import KNeighborsClassifier
+from sklearn.datasets import load_iris
+
 import pandas as pd
 import numpy as np
 from sklearn.utils import resample
 from sklearn import preprocessing
 import matplotlib.pyplot as plt
 import sklearn
-from sklearn import metrics
-from sklearn.metrics import accuracy_score
-from sklearn.metrics import f1_score
-from sklearn.preprocessing import QuantileTransformer
-from sklearn.metrics import accuracy_score
-from sklearn.metrics import confusion_matrix, ConfusionMatrixDisplay
-from sklearn import metrics
-from sklearn.metrics import precision_score
-from sklearn.metrics import recall_score
-from sklearn.metrics import f1_score
-from tensorflow.keras.utils import to_categorical, plot_model
-import tensorflow as tf
-from tensorflow import keras
-from keras.models import Sequential
-from keras.layers import Dense,GRU,Embedding,Dropout,Flatten,Conv1D,MaxPooling1D,LSTM
+
+#from tensorflow.keras.utils import to_categorical, plot_model
+#import tensorflow as tf
+#from tensorflow import keras
+#from keras.models import Sequential
+#from keras.layers import Dense,GRU,Embedding,Dropout,Flatten,Conv1D,MaxPooling1D,LSTM
 from sklearn.svm import SVC
-from sklearn.linear_model import LogisticRegression, SGDClassifier
-from sklearn.neighbors import KNeighborsClassifier
-from sklearn.model_selection import train_test_split
-import joblib
 import seaborn as sns
-from sklearn.datasets import load_iris
 
 
 def train_and_eval(
